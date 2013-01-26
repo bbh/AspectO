7b7a8c47c2af6c7d0555913542c67bfe7a57267b
Basilio Briceño <bbh@briceno.mx>
2013-01-26 00:41:42 -0600

    fixing code style

--- aspecto/AspectO.php
+++ aspecto/AspectO.php
@@ -12,65 +12,85 @@
  * @version 0.1.7
  * @todo aspect->class/method weaving
  */
-class AspectO extends Overload
-{
-    public function __construct ( AspectOConfig &$config ) {
-        if ( $config->getMode() && $config->getMode() == 'debug' ) {
-            $this->WeaveAspects( $config, $this->ValidateAspects( $this->ParseAspects( $config ) ) );
-        }
+class AspectO extends Overload {
+
+  public function __construct ( AspectOConfig &$config )
+  {
+    if ( $config->getMode() ) {
+
+      $this->WeaveAspects( $config, $this->ValidateAspects( $this->ParseAspects( $config ) ) );
     }
-    /**
-     * Returns parsed Aspect objects
-     *
-     * @param AspectOConfig $config
-     * @return Mixed
-     */
-    public function ParseAspects ( AspectOConfig $config ) {
-        $aspects = glob( $config->getAspectsPath() . '*.php' );
-        for ( $i = 0; $i < count( $aspects ); $i++ ) {
-            $aspect = new AspectOParser( $aspects[$i], $config );
-            $aspects[$i] = $aspect->getAspect();
-        }
-        if ( is_array( $aspects ) && count( $aspects ) <= 1 ) {
-            foreach ( $aspects as $aspect ) {
-                $aspects = $aspect;
-            }
-        }
-        return $aspects;
+  }
+
+  /**
+   * Returns parsed Aspect objects
+   *
+   * @param AspectOConfig $config
+   * @return Mixed
+   */
+  public function ParseAspects ( AspectOConfig $config )
+  {
+    $aspects = glob( $config->getAspectsPath() . '*.php' );
+
+    for ( $i = 0; $i < count( $aspects ); $i++ ) {
+
+      $aspect = new AspectOParser( $aspects[$i], $config );
+
+      $aspects[$i] = $aspect->getAspect();
     }
 
-    /**
-     * Validates Aspect objects
-     *
-     * @param Mixed $aspects
-     * @return Mixed
-     */
-    public function ValidateAspects( $aspects ) {
-        if ( is_array( $aspects ) ) {
-            foreach ( $aspects as $aspect ) {
-                $validate = new AspectOValidator( $aspect );
-            }
-        } else {
-            $validate = new AspectOValidator( $aspects );
-        }
-        return $aspects;
+    if ( is_array( $aspects ) && count( $aspects ) <= 1 ) {
+
+      foreach ( $aspects as $aspect ) {
+
+        $aspects = $aspect;
+      }
     }
 
-    /**
-     * Weaves Aspects
-     *
-     * @param AspectOConfig $config
-     * @param Mixed $aspect
-     */
-    protected function WeaveAspects( AspectOConfig &$config, $aspects )
-    {
-        if ( is_array( $aspects ) && count( $aspects ) > 1 ) {
-            foreach ( $aspects as $aspect ) {
-                self::WeaveAspects( $config, $aspect );
-            }
-        } else {
-            $weaver = new AspectOWeaver( $config, $aspects );
-        }
+    return $aspects;
+  }
+
+  /**
+   * Validates Aspect objects
+   *
+   * @param Mixed $aspects
+   * @return Mixed
+   */
+  public function ValidateAspects( $aspects )
+  {
+    if ( is_array( $aspects ) ) {
+
+      foreach ( $aspects as $aspect ) {
+
+        $validate = new AspectOValidator( $aspect );
+      }
+
+    } else {
+
+      $validate = new AspectOValidator( $aspects );
     }
+
+    return $aspects;
+  }
+
+  /**
+   * Weaves Aspects
+   *
+   * @param AspectOConfig $config
+   * @param Mixed $aspect
+   */
+  protected function WeaveAspects( AspectOConfig &$config, $aspects )
+  {
+    if ( is_array( $aspects ) && count( $aspects ) > 1 ) {
+
+      foreach ( $aspects as $aspect ) {
+
+        self::WeaveAspects( $config, $aspect );
+      }
+
+    } else {
+
+      $weaver = new AspectOWeaver( $config, $aspects );
+    }
+  }
 }
-?>
